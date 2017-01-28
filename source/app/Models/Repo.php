86aec4770051c5d828293e8006d866a8cf227620
kevin kawaguchi <kevinkawaguchi@gmail.com>
2017-01-27 18:13:56 -0800

    Add initial Laravel Models

---
+++ source/app/Models/Repo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Repo extends Model
+{
+	use SoftDeletes;
+
+	protected $table = 'repos';
+
+	protected $fillable = [
+		'name',
+	];
+}
