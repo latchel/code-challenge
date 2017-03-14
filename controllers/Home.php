af170d20dc3a2e8f1eee0296baa4ac071e2ffae5
Jullian Chavez <jullian.chavez@gmail.com>
2017-03-14 13:10:29 -0700

    Adding home controller

--- controllers/Home.php
+++ controllers/Home.php
@@ -3,9 +3,33 @@
 namespace Latchel;
 
 use Controller;
+use Post;
+use User;
+use Comment;
 
 class HomeController extends Controller{
     public function index(){
-        return view('template');
+        $posts = $this->getPosts('home');
+
+        return view('template', ['posts' = $posts]);
     }
+
+    public static function getPosts($slug){
+        $posts = Post::where('slug', '=', $slug)->get();
+
+        foreach($posts as &$post){
+            $post->user = User::find($post->user_id);
+        }
+
+        foreach($posts as &$post){
+            $post->comments = Comment::where('post_id', '=', $post->post_id)->get();
+
+            foreach($post->comments as &$comment){
+                $comment->user = User::find($comment->user_id);
+            }
+        }
+
+        return $posts;
+    }
+
 }
