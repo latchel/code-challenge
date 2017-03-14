15eadeebcd32cfa0bea47f634b2fb60a471e81ee
Jullian Chavez <jullian.chavez@gmail.com>
2017-03-14 13:22:15 -0700

    Adding angular directive for content post

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,23 @@
-angular.module('CodeReviewApp', []);
+angular.module('CodeReviewApp', [])
+    .directive('post', [function(){
+        return {
+            restrict: 'A',
+            replace: true,
+            transclude: true,
+            template:   '<a ng-href="/posts/{{postid}}">' +
+                            '<div class="post__user" ng-click="toggleCollapse()">{{username}}</div>' +
+                            '<div class="post" ng-class="{\'post--collapsed\': collapsed}"></div>' +
+                        '</a>',
+            scope: {
+                postid: '@',
+                username: '@'
+            },
+            controller: ['$scope', function($scope){
+                $scope.collapsed = true;
+
+                $scope.toggleCollapse = function(){
+                    $scope.collapsed = !$scope.collapsed;
+                };
+            }]
+        }
+    }]);
